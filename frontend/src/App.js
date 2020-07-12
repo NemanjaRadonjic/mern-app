180acf6b4731e92b1893b556cf2f8e6ff532111f
Nemanja <enravel314@gmail.com>
2020-07-12 01:51:15 +0100

    basic routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { Router, Redirect } from '@reach/router';
+
+import Home from './routes/Home';
+
+import NotFound from './routes/NotFound';
+
 function App() {
   return (
     <div className="App">
-      MERN-APP
+      <Router>
+        <Redirect from="/" to="/home" noThrow/>
+        <Home path="home"/>
+
+        <NotFound default />
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/routes/NotFound/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFound() {
+    return <div>Sorry, nothing to see here...</div>
+}
+
+export default NotFound;
